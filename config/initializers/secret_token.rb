a6c29fb93fe6f946f38033ce9e6bbfbf266658fa
Rishik Dhar <remotemethod@gmail.com>
2013-09-06 01:10:34 -0700

    Initialized the reporsitory with skeleton rails app Caterpillar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caterpillar::Application.config.secret_token = 'adf76bfca7e4a259a80ba185c9e6e6e3c43c5a9453c54d8af111e63d4fd697955b6f587668742b68fd735c30efbbfbdc2bfeb9439fe835e6968cadfd5da08a0b'
